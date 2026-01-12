43d3439419764c2e71c7d828248cdcaa165675c5
knjoroge-casa <knjoroge@gmail.com>
2026-01-12 13:17:28 +0300

    Create postcss.config.js

---
+++ postcss.config.js
@@ -0,0 +1,25 @@
+export default {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+}
+```
+
+---
+
+## File 8: `.gitignore`
+```
+# Dependencies
+node_modules
+
+# Production
+dist
+
+# Misc
+.DS_Store
+*.log
+
+# Environment
+.env
+.env.local
