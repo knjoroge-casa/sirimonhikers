cbe60e04637a2505e34a735f8cd885dbba77bbe2
knjoroge-casa <knjoroge@gmail.com>
2026-04-23 17:55:16 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
 import { Analytics } from '@vercel/analytics/react'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-    <Analytics />
+    <BrowserRouter>
+      <App />
+      <Analytics />
+    </BrowserRouter>
   </React.StrictMode>,
 )
